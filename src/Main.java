494a740c99424cf9f4277acc1fca9e0c96566de6
Spyro <124052906+mdspyro@users.noreply.github.com>
2023-10-08 17:23:56 +0400

    change

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         // Начальная сумма
-        int x = 300;
+        int balance = 300;
 
         // Сумма пополнения
-        int x2 = 2000;
+        int refill = 2000;
 
         // Минимум для начисления бонуса
-        int x3 = 1000;
+        int minThreshold = 1000;
 
         // Количество бонусов
-        int x4 = 0;
+        int bonus = 0;
 
         // Проверка превышения порога
-        if (x2 > x3) {
+        if (refill > minThreshold) {
             // Рассчет бонуса
-            int x5 = x2 - x3;
-            int x6 = (int) (x5 / 100);
-            x4 = x6 * 1;
+            int calcBonus = refill - minThreshold;
+            int totalBonus = (int) (calcBonus / 100);
+            bonus = totalBonus * 1;
         }
 
         // Итоговое количество бонусов
-        int x7 = x + x2 + x4;
+        int x7 = balance + refill + bonus;
 
         System.out.println("Счет абонента: " + x7);
-        System.out.println("Зачисленные бонусы: " + x4);
+        System.out.println("Зачисленные бонусы: " + bonus);
     }
 }
